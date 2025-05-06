4dfa89cbae0acab788a5ad65d94a4feffebcdd1a
Hiroki Sasano <hisasano@c2r1s5.42tokyo.jp>
2025-05-06 19:05:00 +0900

    5_6libft_2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,33 +6,34 @@
 /*   By: hisasano <hisasano@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 20:33:58 by hisasano          #+#    #+#             */
-/*   Updated: 2025/05/06 17:15:50 by hisasano         ###   ########.fr       */
+/*   Updated: 2025/05/06 19:00:34 by hisasano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
 #include <stdlib.h>
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len;
 	size_t	i;
 	size_t	j;
 	char	*str;
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	i = 0;
-	j = 0;
-	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (ft_strlen(s1) > SIZE_MAX - ft_strlen(s2) - 1)
+		return (NULL);
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!str)
 		return (NULL);
+	i = 0;
 	while (s1[i] != '\0')
 	{
 		str[i] = s1[i];
 		i++;
 	}
+	j = 0;
 	while (s2[j] != '\0')
 	{
 		str[i + j] = s2[j];
